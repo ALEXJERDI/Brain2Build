c2ed85377eb28d21e23cbfbbf4ee65691aab5ce9
Jaafar-Hadeg <Jaafar.Hadeg@emsi-edu.ma>
2025-10-28 17:34:59 +0100

    room service /room member service/  ajout Long roomid   danss roommembercreateupdate et ajout de la methode qui permet la recherche des member selon roomid     List<RoomMember> findByRoom_Id(Long roomId);

--- src/main/java/com/example/brain2build/repository/RoomMemberRepository.java
+++ src/main/java/com/example/brain2build/repository/RoomMemberRepository.java
@@ -6,6 +6,7 @@ import com.example.brain2build.domain.entity.Worker;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,4 +18,7 @@ public interface RoomMemberRepository extends JpaRepository<RoomMember, Long> {
     Optional<RoomMember> findByRoomAndWorker(Room room, Worker worker);
 
     List<RoomMember> findByWorker_Id(Long workerId);
+    List<RoomMember> findByRoom_Id(Long roomId);
+
+
 }
