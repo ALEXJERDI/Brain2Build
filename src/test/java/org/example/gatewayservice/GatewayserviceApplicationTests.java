c2b3d7379cce365b160f1df7d133f1c4a0d767e8
lordanas001 <anashalim100@gmail.com>
2025-11-18 23:54:34 +0100

    newgatway

--- src/test/java/org/example/gatewayservice/GatewayserviceApplicationTests.java
+++ src/test/java/org/example/gatewayservice/GatewayserviceApplicationTests.java
@@ -1,13 +1,1 @@
-package org.example.gatewayservice;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class GatewayserviceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
